2b3ee33fe4152147e8ff2a566377bd524f81ca9c
ben3a <ahmed.ben.alaya.97@gmail.com>
2021-03-18 19:02:04 +0100

    ahmed first commit

---
+++ SIRH/Models/Candidature.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SIRH.Models
+{
+    public class Candidature
+    {
+        public int Id { get; set; }
+        [Required]
+        public DateTime JobInterviewDate { get; set; }
+
+        [Required]
+        public DateTime CandidatureDate { get; set; }
+
+    }
+}

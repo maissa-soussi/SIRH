b4e7bee172c254e83a47ce289c3c4893003ae314
Maissa Soussi <maissa.soussi1920@yahoo.com>
2021-03-18 16:45:18 +0100

    Other Model

---
+++ SIRH/Models/Other.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SIRH.Models
+{
+    public class Other
+    {
+        public int Id { get; set; }
+
+        
+        [Url]
+        public string FacebookUrl { get; set; }
+
+        
+        [Url]
+        public string LinkedinUrl { get; set; }
+    }
+}

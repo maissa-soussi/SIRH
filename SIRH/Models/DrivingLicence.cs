2fa1764a898a667017eece4d45780a126e7b000a
Maissa Soussi <maissa.soussi1920@yahoo.com>
2021-03-18 16:38:35 +0100

    DrivingLicence Model

---
+++ SIRH/Models/DrivingLicence.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SIRH.Models
+{
+    public class DrivingLicence
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Type { get; set; }
+    }
+}

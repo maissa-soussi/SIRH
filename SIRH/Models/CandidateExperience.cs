0cd9570c4c7bdaf04b358cf8c73ab16b9cd464ff
farouk kassebi <farouk.kassebi@ensi-uma.tn>
2021-03-19 22:02:24 +0100

    add joboffer experience country contrattype candidateexperience currency Models

---
+++ SIRH/Models/CandidateExperience.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SIRH.Models
+{
+    public class CandidateExperience
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string CompanyName { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Profession { get; set; }
+        [Required]
+        [StringLength(10)]
+        public string StartDate { get; set; }
+        [Required]
+        [StringLength(10)]
+        public string EndDate { get; set; }
+    }
+}
---
+++ SIRH/Models/JobOffer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SIRH.Models
+{
+    public class JobOffer
+    {
+        public int Id { get; set; }
+        public string Reference { get; set; }
+        public string Description { get; set; }
+        [Required]
+        public int MinSalary { get; set; }
+        [Required]
+        public int MaxSalary { get; set; }
+        [Required]
+        [StringLength(10)]
+        public string PublicationDate { get; set; }
+        [Required]
+        [StringLength(10)]
+        public string ExpirationDate { get; set; }
+    }
+}

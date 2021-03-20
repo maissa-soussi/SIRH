e8e5985e5e1afa2a9080b476b8b4daaa3198e345
ahmedbenalaya30 <ahmed.benalaya@ensi-uma.tn>
2021-03-20 13:53:49 +0100

    Candidate Modele

--- SIRH/Models/Candidature.cs
+++ SIRH/Models/Candidature.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -14,6 +15,17 @@ namespace SIRH.Models
 
         [Required]
         public String CandidatureDate { get; set; }
+
+        [Required]
+        [ForeignKey("JobOffer")]
+        public int? JobOfferId { get; set; }
+        public virtual JobOffer JobOffer { get; set; }
+
+        [Required]
+        [ForeignKey("Candidate")]
+        public int? CandidateId { get; set; }
+        public virtual Candidate Candidate { get; set; }
+
 
     }
 }

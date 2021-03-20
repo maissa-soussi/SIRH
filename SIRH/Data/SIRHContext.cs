a02ebaa7103ce0ffa6135af5feb8748f337d4bb8
Maissa Soussi <maissa.soussi1920@yahoo.com>
2021-03-20 02:11:07 +0100

    User Controller & Migration

---
+++ SIRH/Data/SIRHContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SIRH.Models;
+
+namespace SIRH.Data
+{
+    public class SIRHContext : DbContext
+    {
+        public SIRHContext (DbContextOptions<SIRHContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SIRH.Models.User> User { get; set; }
+    }
+}
--- SIRH/Startup.cs
+++ SIRH/Startup.cs
@@ -10,6 +10,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SIRH.Data;
 
 namespace SIRH
 {
@@ -26,6 +28,10 @@ namespace SIRH
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddControllers().AddNewtonsoftJson(options => options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+
+            services.AddDbContext<SIRHContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("SIRHContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
